80063d79a5dd2dcd9957d6e5f8faef78a89e37c8
carsonSgit <carsonspriggs6@gmail.com>
2025-11-26 17:12:44 -0500

    consistency in hover links

--- app/page.tsx
+++ app/page.tsx
@@ -155,7 +155,7 @@ export default function Home() {
 									href="https://www.instagram.com/lokkoluv/"
 									target="_blank"
 									rel="noopener noreferrer"
-									className="flex items-center gap-5 group hover:opacity-70 transition-opacity"
+									className="flex items-center gap-5 group"
 								>
 									<div className="w-[50px] h-[48px] flex items-center justify-center">
 										<svg
@@ -170,7 +170,7 @@ export default function Home() {
 											<path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"></path>
 										</svg>
 									</div>
-									<span className="hidden md:inline text-[clamp(1.5rem,3vw,2.25rem)] tracking-[2.16px] text-black">
+									<span className="hidden md:inline text-3xl tracking-[2.16px] text-black group-hover:underline underline-offset-4">
 										INSTAGRAM
 									</span>
 								</a>
@@ -179,7 +179,7 @@ export default function Home() {
 									href="https://www.tiktok.com/@lokkoluv"
 									target="_blank"
 									rel="noopener noreferrer"
-									className="flex items-center gap-5 group hover:opacity-70 transition-opacity"
+									className="flex items-center gap-5 group"
 								>
 									<div className="w-[50px] h-[48px] flex items-center justify-center">
 										<svg
@@ -194,7 +194,7 @@ export default function Home() {
 											<path d="M224,72a48.05,48.05,0,0,1-48-48,8,8,0,0,0-8-8H128a8,8,0,0,0-8,8V156a20,20,0,1,1-28.57-18.08A8,8,0,0,0,96,130.69V88a8,8,0,0,0-9.4-7.88C50.91,86.48,24,119.1,24,156a76,76,0,0,0,152,0V116.29A103.25,103.25,0,0,0,224,128a8,8,0,0,0,8-8V80A8,8,0,0,0,224,72Zm-8,39.64a87.19,87.19,0,0,1-43.33-16.15A8,8,0,0,0,160,102v54a60,60,0,0,1-120,0c0-25.9,16.64-49.13,40-57.6v27.67A36,36,0,1,0,136,156V32h24.5A64.14,64.14,0,0,0,216,87.5Z"></path>
 										</svg>
 									</div>
-									<span className="hidden md:inline text-[clamp(1.5rem,3vw,2.25rem)] tracking-[2.16px] text-black">
+									<span className="hidden md:inline text-3xl tracking-[2.16px] text-black group-hover:underline underline-offset-4">
 										TIKTOK
 									</span>
 								</a>
